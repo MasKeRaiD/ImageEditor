0f976f6011f67a105c95ff1da09c02aaf76994f1
Marius Kowal <marius.kowal@haw-hamburg.de>
2015-05-07 11:16:50 +0200

    Main

--- ImageEditorFrame.java
+++ ImageEditorFrame.java
@@ -8,5 +8,8 @@ import javax.imageio.*;
 	public ImageEditorFrame(){ 
  setDefaultCloseOperation(EXIT_ON_CLOSE);
  setSize(400, 300); setVisible(true); 
-	} 
+	}
+
+public static void main(String[] args){
+}	
  }
